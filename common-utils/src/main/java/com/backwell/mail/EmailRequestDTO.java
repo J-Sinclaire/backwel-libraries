0695461078de549ea1c2bc470689123c06ba0113
J-Sinclaire <gonzalez.ortiz.kenneth.xe@gmail.com>
2026-07-04 13:20:39 -0600

    Add boilerplate code to EmailRequestDTO.java

--- common-utils/src/main/java/com/backwell/mail/EmailRequestDTO.java
+++ common-utils/src/main/java/com/backwell/mail/EmailRequestDTO.java
@@ -18,4 +18,29 @@ public class EmailRequestDTO implements Serializable {
 
     @NotNull
     private Map<String, Object> variables;
+
+    public EmailRequestDTO() {}
+
+    public EmailRequestDTO(String to, String subject, String templateName, Map<String, Object> variables) {
+        this.to = to;
+        this.subject = subject;
+        this.templateName = templateName;
+        this.variables = variables;
+    }
+
+    public String getTo() {
+        return to;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public String getTemplateName() {
+        return templateName;
+    }
+
+    public Map<String, Object> getVariables() {
+        return variables;
+    }
 }
