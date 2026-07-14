b2e475e69f278c33aa6b4f24338301ddd95219af
J-Sinclaire <gonzalez.ortiz.kenneth.xe@gmail.com>
2026-07-13 20:10:52 -0600

    Add support for ROLE_ID token claim, deprecate AUTH_PROVIDER claim favoring the use of the new AUTH_PROVIDERS claim in the move towards a muli auth-provider implementation.

--- common-utils/src/main/java/com/backwell/enums/JwtClaim.java
+++ common-utils/src/main/java/com/backwell/enums/JwtClaim.java
@@ -5,10 +5,10 @@ package com.backwell.enums;
  * contenidos dentro de los Tokens de JWT emitidos por el sistema.
  * <p>
  * Cada constante mapea un atributo legible a su correspondiente clave de texto
- * literal utilizada en la carga útil (payload) del JSON Web Token.
+ * literal utilizada en la carga útil del JSON Web Token.
  * </p>
  *
- * @version 2.0.0
+ * @version 2.1.0
  */
 public enum JwtClaim {
 
@@ -25,17 +25,32 @@ public enum JwtClaim {
      * * @deprecated Reemplazado por {@link #ROLE} debido al cambio a un esquema
      * de rol único por usuario en la versión 2.0.0.
      */
-    @Deprecated
+    @Deprecated(forRemoval = true)
     ROLES("roles"),
 
-    /** Rol principal asignado al usuario. */
+
+    /** UUID del rol asignado al usuario*/
+    ROLE_ID("role-id"),
+
+    /** Rol asignado al usuario.*/
     ROLE("role"),
 
-    /** Representación codificada de los permisos asociados al usuario (máscara de bits). */
+    /** Representación codificada de los permisos asociados al usuario (máscara de bits).
+     * @since 2.0.0*/
     PERMISSIONS("permissions"),
 
-    /** El proveedor a través del cual se autenticó el usuario. Ver {@link AuthProvider}. */
-    AUTH_PROVIDER("auth-provider");
+
+    /** El proveedor a través del cual se autenticó el usuario. Ver {@link AuthProvider}.
+     * @deprecated Replaced in favor of using {@link #AUTH_PROVIDERS} in the move towards an implementation with
+     * more than one authentication provider per user. Deprecated as of version 2.1.0
+     * */
+    @Deprecated
+    AUTH_PROVIDER("auth-provider"),
+
+    /**
+     * Lista de proveedores de autenticación del usuario en formato String
+     * @since 2.1.0*/
+    AUTH_PROVIDERS("auth-providers");
 
     /** Representación textual de la clave del claim en el JSON del JWT. */
     private final String key;
